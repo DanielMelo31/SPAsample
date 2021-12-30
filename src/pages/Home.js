4f27c410b0e648bccab27dc89df9752c5e420e5d
Daniel Melo <danielmelo276@gmail.com>
2021-12-29 22:04:46 -0500

    routes

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+	const view = `
+        <div class="characters">
+            <article class=""character__item>
+                <a href="#/1">
+                    <img src="character__name" alt="name">
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+    `;
+
+	return view;
+};
+
+export { Home };
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,31 @@
-import { webpack } from "webpack";
-import path from webpack
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, dist),
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }    
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin([
-            {
-                inject: true,
-                template: './public/index.html',
-                filename: './index.html' 
-            }
-        ])
-    ]
-}
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+	],
+};
