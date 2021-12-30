aeae167a144eca205f06994edad1affaa08db1e3
Daniel Melo <danielmelo276@gmail.com>
2021-12-29 19:25:36 -0500

    project body

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import { webpack } from "webpack";
+import path from webpack
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, dist),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }    
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html' 
+            }
+        ])
+    ]
+}
